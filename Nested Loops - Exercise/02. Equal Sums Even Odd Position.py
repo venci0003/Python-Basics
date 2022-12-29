43cf0f089a2d80e1f618d96bafd2ab2cf8a73dbd
venci0003 <venci0003@gmail.com>
2022-12-29 19:31:01 +0200

    Uploaded solutions to - Nested Loops - Exercise

---
+++ Nested Loops - Exercise/02. Equal Sums Even Odd Position.py
@@ -0,0 +1,37 @@
+number_one = int(input())
+number_two = int(input())
+
+for number in range(number_one, number_two + 1):
+    number_to_str = str(number)
+    even_sum = 0
+    odd_sum = 0
+
+    for index, digit in enumerate(number_to_str):
+        if index % 2 == 0:
+            even_sum += int(digit)
+        else:
+            odd_sum += int(digit)
+    if even_sum == odd_sum:
+        print(number, end=" ")
+
+
+
+# [print(num, end=" ") for num in range(int(input()), int(input()) + 1) if sum(list(map(int, str(num)))[::2]) == sum(list(map(int, str(num)))[1::2])]
+
+
+
+
+###
+# number_one = int(input())
+# number_two = int(input())
+#
+# for number in range(number_one, number_two + 1):
+#     even_sum = 0
+#     odd_sum = 0
+#     for i, digit in enumerate(str(number)):
+#         if i % 2 == 0:
+#             even_sum += int(digit)
+#         else:
+#             odd_sum += int(digit)
+#     if even_sum == odd_sum:
+#         print(number, end=" ")
---
+++ Nested Loops - Exercise/05. Special Numbers.py
@@ -0,0 +1,55 @@
+number_to_check = int(input())
+
+
+
+for number_one in range(1, 9):
+    if number_to_check % number_one == 0:
+
+        for number_two in range(1, 9):
+            if number_to_check % number_two == 0:
+
+                for number_three in range(1, 9):
+                    if number_to_check % number_three == 0:
+
+                        for number_four in range(1, 9):
+                            if number_to_check % number_four == 0:
+                                print(f"{number_one}{number_two}{number_three}{number_four}", end=" ")
+
+
+
+#
+# number_to_check = int(input())
+#
+# for n in range(1111, 10_000):
+#     if all(x != "0" and number_to_check % int(x) == 0 for x in str(n)):
+#         print(n, end=" ")
+#
+#
+#
+#
+
+
+
+
+###
+# n = int(input())
+#
+# for numbers in range(1111, 10000):
+#     magic_number_find = True
+#     for number in str(numbers):
+#         if any(["0" in str(numbers), n % int(number) != 0]):
+#             magic_number_find = False
+#             break
+#     if magic_number_find:
+#         print(numbers, end=" ")
+
+###
+
+# n = int(input())
+#
+# for numbers in range(1111, 10000):
+#     numbers = str(numbers)
+#     if "0" not in numbers:
+#         if all([n % int(numbers[0]) == 0, n % int(numbers[1]) == 0, n % int(numbers[2]) == 0,
+#                 n % int(numbers[3]) == 0]):
+#             print(numbers, end=" ")
